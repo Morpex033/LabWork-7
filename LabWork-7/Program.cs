26874e4240f41d80430d674cb88c0d9848ac4957
Andrey Sharipov <andrey.sharipov1@gmail.com>
2024-04-12 13:01:19 +0300

    Update commit

--- LabWork-7/Program.cs
+++ LabWork-7/Program.cs
@@ -6,20 +6,6 @@ namespace Lab_Work_7
     {
         public static void Main(string[] args)
         {
-            LabWork7();
-
-            //Загрузка словаря из сохраненого файла в методе LabWork7()
-            StudentScores studentScores1 = StudentDataStorage.Load();
-
-            studentScores1.PrintDictionary();
-        }
-
-        /*
-            Статический метод LabWork7 это часть кода из Лабораторной роботы 7,
-            который был вынесен в отдельный метод для удобста демонстрации загрузки словаря из файла.
-        */
-        public static void LabWork7()
-        {
             StudentScores studentScores = new StudentScores();
 
             // Добавляем оценки
@@ -29,16 +15,13 @@ namespace Lab_Work_7
             studentScores.AddScore("Bob", "Math", 80);
             studentScores.AddScore("Bob", "Physics", 70);
 
-            // Вывод баллов студентов
-            studentScores.PrintStudentScore("Alice");
-            studentScores.PrintStudentScore("Bob");
-            //Переопределенный ToString
-            Console.WriteLine(studentScores);
-            Console.WriteLine("----------------------------------------------------");
-            // Вывод словаря
-            studentScores.PrintDictionary();
             // Сохранение словаря
             StudentDataStorage.Save(studentScores);
+
+            //Загрузка словаря из сохраненого файла
+            StudentScores studentScores1 = StudentDataStorage.Load();
+
+            studentScores1.PrintDictionary();
         }
     }
 }
